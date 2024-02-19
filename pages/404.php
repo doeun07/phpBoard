e394c9f8bb839cba9561353c590907a64ca89218
최도은 <choidoeun0102@gmail.com>
2024-02-19 20:23:15 +0900

    에러 페이지 생성

---
+++ pages/404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>잘못된 접근</title>
+    <link rel="stylesheet" href="./pages/style.css">
+</head>
+<body>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        html, body {
+            width: 100%;
+            height: 100%;
+        }
+        #container{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        h2{
+            color: red;
+        }
+    </style>
+    <div id="container">
+        <h1>이도저도 아무것도 아닌 그러하지 못한 그런 곳</h1>
+        <h2>404</h2>
+    </div>
+</body>
+</html>
