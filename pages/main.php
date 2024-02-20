e9153cda158af138a7aa6a0a52221ac123cde027
최도은 <choidoeun0102@gmail.com>
2024-02-20 17:21:03 +0900

    로그아웃 기능 구현

--- pages/main.php
+++ pages/main.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+if(isset($_SESSION["user_idx"])) {
+    echo "로그인 됨";
+} else {
+    echo "로그아웃 됨";
+};
+
+function logout() {
+    session_destroy();
+}
+
+?>
 <h1>Root Directory Access</h1>
 <a href="register">회원가입</a>
 <a href="login">로그인</a>
+<form action="" method="POST">
+    <button type="submit" onclick="<?php echo logout(); ?>">로그아웃</button>
+</form>
