0f871115287f9dcd67d5e55317efaccb4c59d7ca
최도은 <choidoeun0102@gmail.com>
2024-02-21 15:05:12 +0900

    css 공통 적용 되도록 index 수정

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="./css/global.css">
-<link rel="stylesheet" href="./css/style.css">
 <?php
 include('./config/dbconnect.php');
 session_start();
@@ -10,8 +8,19 @@ $path[1] = isset($path[1]) ? $path[1] : null;
 $resource = explode("/", $path[0]);
 echo "<script>console.log('path[0] = " . $path[0] . "');</script>";
 echo "<script>console.log('path[1] = " . $path[1] . "');</script>";
+?>
 
-
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Router</title>
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<?php
 $page = "";
 include('./components/header.php');
 switch ($resource[1]) {
@@ -33,3 +42,6 @@ switch ($resource[1]) {
 }
 include($page);
 ?>
+    
+</body>
+</html>
