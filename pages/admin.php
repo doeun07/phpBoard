55f1f81263c012a746df64eb27c1a93a29e8ff52
최도은 <choidoeun0102@gmail.com>
2024-02-22 16:22:25 +0900

    게시글 삭제 기능 구현

--- pages/admin.php
+++ pages/admin.php
@@ -23,14 +23,15 @@ $stmt->execute();
 $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "<script>
-    alert('게시글을 삭제하시겠습니까?');
-    </script>";
+    $sql = "UPDATE posts SET is_deleted = 1 WHERE post_idx = :post_idx";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':post_idx', $_POST["post_idx"]);
+    $stmt->execute();
 }
 ?>
 
 <div class="container posts_container">
-    <h2>게시글 목록</h2>
+    <h2>관리자 페이지</h2>
     <?php
     if ($posts) {
         foreach ($posts as $post) {
@@ -41,18 +42,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
             
             $divinnertext = "";
-            $divinnertext .= "<form action='admin' method='post'>";
             $divinnertext .= "<div class='post container'>";
             $divinnertext .= "<h3>" . $post["title"] . "</h3>";
             $divinnertext .= "<p class='textsm'>작성자 ID : " . $user["username"] . "</p>";
             $divinnertext .= "<p class='textsm'>게시글 ID : " . $post["post_idx"] . "</p>";
             $divinnertext .= "<p class='textsm'>작성일 : " . $post["write_date"] . "</p>";
             $divinnertext .= "<p class='textmd'>" . $post["content"] . "</p>";
-            $divinnertext .= "<button type='submit'>게시글 삭제하기</button>";
+            $divinnertext .= "<button id='" . $post["post_idx"] . "' onClick='postDelete(this);'>게시글 삭제하기</button>";
             $divinnertext .= "</div>";
-            $divinnertext .= "</form>";
             echo $divinnertext;
         }
     }
     ?>
 </div>
+<script>
+    function postDelete(elem){
+        const post_idx = elem.id;
+        const isConfirm = confirm(`정말로 ${post_idx}번 게시글을 삭제하시겠습니까?`);
+        if(isConfirm) {
+            $.ajax({
+                url: './admin',
+                type: 'POST',
+                data: {"post_idx": post_idx},
+                success: function() {
+                    alert("정상적으로 게시글이 삭제되었습니다.");
+                    location.href = './admin';
+                },
+                error: function() {
+                    alert("게시글 삭제 중 문제가 발생하였습니다.");
+                }
+            })
+        } else {
+            alert("게시글 삭제가 취소되었습니다.");
+        }
+        return console.log(elem);
+    }
+</script>
