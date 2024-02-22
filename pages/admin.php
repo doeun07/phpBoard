d939fda5e1510545f94ad3f45c5d8ab8f9f5fe2b
최도은 <choidoeun0102@gmail.com>
2024-02-22 14:42:31 +0900

    관리자 페이지 수정 게시물 띄우기 게시글 삭제 버튼 눌렀을 때 alert 띄우기

--- pages/admin.php
+++ pages/admin.php
@@ -1,8 +1,6 @@
 <?php
 //관리자 검증
-if(!empty($_SESSION["is_admin"]) && $_SESSION["is_admin"] == 1) {
-    echo "관리자 페이지";
-} else {
+if(!empty($_SESSION["is_admin"]) && $_SESSION["is_admin"] == 0) {
     echo "
     <script>
     alert('관리자만 이용가능한 페이지 입니다.')
@@ -10,4 +8,42 @@ if(!empty($_SESSION["is_admin"]) && $_SESSION["is_admin"] == 1) {
     </script>
     ";
 }
+$sql = "SELECT * FROM posts WHERE is_deleted = 0 ORDER BY write_date DESC"; //ASC = 오름차순, DESC = 내림차순
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<script>
+    alert('게시글을 삭제하시겠습니까?');
+    </script>";
+}
 ?>
+
+<div class="container posts_container">
+    <h2>게시글 목록</h2>
+    <?php
+    if ($posts) {
+        foreach ($posts as $post) {
+            $sql = "SELECT * FROM users WHERE user_idx = :user_idx";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":user_idx", $post["user_idx"]);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $divinnertext = "";
+            $divinnertext .= "<form action='admin' method='post'>";
+            $divinnertext .= "<div class='post container'>";
+            $divinnertext .= "<h3>" . $post["title"] . "</h3>";
+            $divinnertext .= "<p class='textsm'>작성자 ID : " . $user["username"] . "</p>";
+            $divinnertext .= "<p class='textsm'>게시글 ID : " . $post["post_idx"] . "</p>";
+            $divinnertext .= "<p class='textsm'>작성일 : " . $post["write_date"] . "</p>";
+            $divinnertext .= "<p class='textmd'>" . $post["content"] . "</p>";
+            $divinnertext .= "<button type='submit'>게시글 삭제하기</button>";
+            $divinnertext .= "</div>";
+            $divinnertext .= "</form>";
+            echo $divinnertext;
+        }
+    }
+    ?>
+</div>
