29fe36921110979635af6ea230319f803b9c4d75
최도은 <choidoeun0102@gmail.com>
2024-02-22 18:06:27 +0900

    게시글 작성 후 posts 페이지로 이동

--- pages/posting.php
+++ pages/posting.php
@@ -1,25 +1,30 @@
 <?php
 //로그인 검증
-if(!isset($_SESSION["user_idx"])) {
+if (!isset($_SESSION["user_idx"])) {
     echo "
     <script>
     alert('로그인 후 이용 가능합니다.')
     location.href = './login';
     </script>
     ";
-} 
+}
 
 //글 작성 로직
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user_idx = $_SESSION["user_idx"];
     $title = $_POST["title"];
     $content = $_POST["content"];
-    
+
     try {
         $sql = "INSERT INTO posts (user_idx, title, content) VALUES (?, ?, ?)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$user_idx, $title, $content]);
-        echo "게시글 작성이 되었습니다.";
+        echo "
+        <script>
+        alert('게시글이 작성되었습니다.')
+        location.href = './posts';
+        </script>
+        ";
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
