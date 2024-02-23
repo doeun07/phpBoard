b9fa29bf944f92219bd1b706414e267fc70d782a
최도은 <choidoeun0102@gmail.com>
2024-02-23 18:45:34 +0900

    게시물 수정 페이지 생성

---
+++ pages/update.php
@@ -0,0 +1,14 @@
+<div class="container posting_container">
+    <h2>게시글 수정</h2>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="title">제목</label>
+            <input type="text" id="title" name="title" required placeholder="제목을 입력하세요.">
+        </div>
+        <div class="form-group">
+            <label for="content">내용</label>
+            <textarea id="content" name="content" required placeholder="내용을 입력하세요."></textarea>
+        </div>
+        <button type="submit">게시글 수정</button>
+    </form>
+</div>
