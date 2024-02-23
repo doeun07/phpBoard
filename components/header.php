3c68f386241ae2b0a7965c5039b785aa851a8c01
최도은 <choidoeun0102@gmail.com>
2024-02-23 13:28:47 +0900

    권한 별 header if문 수정

--- components/header.php
+++ components/header.php
@@ -7,19 +7,15 @@
         <ul>
             <?php
             if(isset($_SESSION["user_idx"])) {
-                if($_SESSION["is_admin"] == 1) {
-                    echo "
-                    <li><a href='./posting'>게시글 작성</a></li>
-                    <li><a href='./posts'>게시글 보기</a></li>
-                    <li><a href='./admin'>관리자 페이지</a></li>
-                    <li><a href='./logout'>로그아웃</a></li>
-                    ";
-                }else{
-                    echo "
+                echo "
                     <li><a href='./posting'>게시글 작성</a></li>
                     <li><a href='./posts'>게시글 보기</a></li>
                     <li><a href='./logout'>로그아웃</a></li>
                     ";
+                    if($_SESSION["is_admin"] == 1) {
+                        echo "
+                        <li><a href='./admin'>관리자 페이지</a></li>
+                        ";
                 }
             } else {
                 echo "
